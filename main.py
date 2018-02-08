b1555eae1b1cc606fee62e1e5f2a01423bb27a5c
Petr Schmied <schmipe5@fit.cvut.cz>
2018-02-08 19:58:08 +0100

    DB: sync archives with YouTube speedup

--- main.py
+++ main.py
@@ -604,25 +604,27 @@ def db_get_archives():
 
 def db_update_archives():
     db = get_db()
-    archived_video_ids = []
+    archived_video_ids_local = db_get_archived()
+    archived_video_ids_remote = []
     unarchive_videos = []
 
     for archive in db_get_archives():
         for video in yt_get_playlist_items(archive['id']):
-            video = yt_get_video(video['snippet']['resourceId']['videoId'])
-            archived_video_ids.append(video['id'])
+            if video['snippet']['resourceId']['videoId'] not in archived_video_ids_local:
+                video = yt_get_video(video['snippet']['resourceId']['videoId'])
+                archived_video_ids_remote.append(video['id'])
 
-            if video['snippet']['channelId'] not in db[flask.session['user']['id']]:
-                db[flask.session['user']['id']][video['snippet']['channelId']] = {
-                    'played': {}, 'archived': {}
-                }
+                if video['snippet']['channelId'] not in db[flask.session['user']['id']]:
+                    db[flask.session['user']['id']][video['snippet']['channelId']] = {
+                        'played': {}, 'archived': {}
+                    }
 
-            if video['id'] not in db[flask.session['user']['id']][video['snippet']['channelId']]['archived']:
-                db[flask.session['user']['id']][video['snippet']['channelId']]['archived'][video['id']] = archive['id']
+                if video['id'] not in db[flask.session['user']['id']][video['snippet']['channelId']]['archived']:
+                    db[flask.session['user']['id']][video['snippet']['channelId']]['archived'][video['id']] = archive['id']
 
     for channel_id, channel in db[flask.session['user']['id']].items():
         for video_id in channel['archived'].keys():
-            if video_id not in archived_video_ids:
+            if video_id not in archived_video_ids_remote:
                 unarchive_videos.append((channel_id, video_id))
 
     for item in unarchive_videos:
